6e1632c1ac9cccdbb2ca952dd8e34ec0bb405279
Triniti Nguyen <tnguyen0614@csu.fullerton.edu>
2025-05-05 03:16:13 -0700

    Create Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import { BsBarChart, BsGear, BsInfoCircle } from 'react-icons/bs';
+import './Header.css';
+
+const Header = ({
+  setIsInfoModalOpen,
+  setIsStatsModalOpen,
+  setIsSettingsModalOpen,
+}) => {
+  return (
+    <header>
+      <div>
+        <button onClick={() => setIsInfoModalOpen(true)}>
+          <BsInfoCircle size="1.6rem" color="var(--color-icon)" />
+        </button>
+      </div>
+      <h1>WORDLE RIVALS</h1>
+      <div>
+        <button onClick={() => setIsSettingsModalOpen(true)}>
+          <BsGear size="1.6rem" color="var(--color-icon)" />
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
