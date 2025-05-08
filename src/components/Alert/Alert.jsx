af10c75f77f5b4647c34a52b66f315e0fb1de748
Triniti Nguyen <tnguyen0614@csu.fullerton.edu>
2025-05-07 18:54:48 -0700

    Create Alert.jsx

---
+++ src/components/Alert/Alert.jsx
@@ -0,0 +1,29 @@
+import useAlert from 'hooks/useAlert';
+import classNames from 'classnames';
+import { CSSTransition } from 'react-transition-group';
+import styles from './Alert.css';
+
+const Alert = () => {
+  const { message, status, isVisible } = useAlert();
+
+  const classes = classNames({
+    [styles.alert]: true,
+    [styles.success]: status === 'success',
+    [styles.error]: status === 'error',
+  });
+
+  return (
+    <div className={styles.alertContainer}>
+      <CSSTransition
+        in={isVisible}
+        timeout={300}
+        classNames={'fadeIn'}
+        unmountOnExit
+      >
+        <span className={classes}>{message}</span>
+      </CSSTransition>
+    </div>
+  );
+};
+
+export default Alert;
